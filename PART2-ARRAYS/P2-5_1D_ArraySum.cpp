0196a162bf1033ede110b31119ddd2bcb5d1f8cb
pinzap_ <151607984+judSoftwareDev@users.noreply.github.com>
2025-09-15 17:55:35 +0800

    Update P2-5_1D_ArraySum.cpp
    running code

--- PART2-ARRAYS/P2-5_1D_ArraySum.cpp
+++ PART2-ARRAYS/P2-5_1D_ArraySum.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int numbers[5] = { 1, 2, 3, 4, 5 };
+    cout << "Sum = " << sumArray(numbers, 5) << endl;
+    return 0;
+}
 
